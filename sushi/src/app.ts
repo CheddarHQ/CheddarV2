475f7769755234fdfd8381ed1b7d2680f674a74d
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-08-19 03:18:33 +0530

    complete revamp hono+trpc

---
+++ sushi/src/app.ts
@@ -0,0 +1,15 @@
+import { Hono } from "hono";
+import { chartRouter } from "./chart";
+import { dataRouter } from "./data";
+import { buyRouter } from "./buy";
+import { logger } from "hono/logger";
+// import { serveStatic } from "hono/bun";
+
+const app = new Hono()
+
+app.use("*", logger());
+
+const apiRoutes = app.basePath("/api").route("buy", buyRouter).route("data", dataRouter).route("chart", chartRouter);
+
+export default app;
+export type App = typeof app;
