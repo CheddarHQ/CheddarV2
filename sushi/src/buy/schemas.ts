da084e8ddc21cd9469bfebd88cbabd445674f01d
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-08-23 04:31:59 +0530

    added tensor & meteora for ftutrre

--- sushi/src/buy/schemas.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const quoteSchema = z.object({
-        inputMint: z.string(),
-        outputMint: z.string(),
-        amount: z.string(), // Query params come as strings
-        slippage: z.string().optional(),
-        platformFees: z.string(),
-    });
