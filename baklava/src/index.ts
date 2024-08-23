8e578fba277859f9af815f3763b10471f0e3f618
Ormon-huehuehue <armnvrma10@gmail.com>
2024-08-24 02:14:09 +0530

    api routes ini index.ts

--- baklava/src/index.ts
+++ baklava/src/index.ts
@@ -10,7 +10,45 @@ const app = new Hono()
 app.use("*", logger());
 
 const apiRoutes = app.basePath("/api")
-                .route("durableSocketServer", durableSocketServer);
+.route("durableSocketServer", durableSocketServer);
+
+
+//TODO : define a zod schema for the message
+// TODO : 
+
+
+//route to send message
+app.post('/api/send-message', async (c) => {
+  const body = await c.req.json();
+
+
+  // Interact with your Durable Object
+  const response = await fetch(`${DURABLE_OBJECT_NAMESPACE}/websocket`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ type,data }),  //get the type from the zod schema (baad me dekhenge) -> z,object before async (c)
+  });
+
+  if (response.ok) {
+    return c.json({ success: true });
+  } else {
+    return c.json({ error: "Failed to send message" }, 500);
+  }
+});
+
+
+//route to fetch the active users
+app.get("/api/active-users", async (c)=>{
+  const response = await axios.get(`${durableSocketServer}/getActiveUsersCount`);
+
+  if(response.status ===200){
+    const data = await response.data;
+    return c.json({activeUser : data})
+  }
+
+})
 
 export default app;
 export type App = typeof app;
