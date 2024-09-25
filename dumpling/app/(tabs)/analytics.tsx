115da1a921a04190c4d52d932b66cb885f1fd86b
Ormon-huehuehue <armnvrma10@gmail.com>
2024-09-25 13:36:12 +0530

    analytics and modals tabs now refetch after every 5 mins

--- dumpling/app/(tabs)/analytics.tsx
+++ dumpling/app/(tabs)/analytics.tsx
@@ -54,6 +54,16 @@ export default function Modal() {
     }
 
     fetchMetadata(initialIds);
+
+
+
+    const intervalId = setInterval(()=>{
+      fetchMetadata(initialIds);
+    }, 300000 )
+
+
+    return () => clearInterval(intervalId);
+
   }, []);
 
   useEffect(() => {
