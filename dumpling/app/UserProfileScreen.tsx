9de0896fd289e0a0c04fb425fc02e12f2f67cff7
Ormon-huehuehue <armnvrma10@gmail.com>
2024-10-04 17:31:48 +0530

    okto partially working

--- dumpling/app/UserProfileScreen.tsx
+++ dumpling/app/UserProfileScreen.tsx
@@ -1,37 +1,69 @@
-import { useEffect, useState } from "react";
-import { View, StyleSheet, Text, Pressable, Button, FlatList } from "react-native";
-import { getUserDetails, getWallets, logout, openOktoBottomsheet } from "rn-okto-sdk";
-// import { User } from "../types/user";
-// import { Wallet } from "../types/wallet";
-import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { useOkto, type OktoContextType, type User,type Wallet } from 'okto-sdk-react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Button } from "react-native";
+
+
 import { NavigationProp } from "@react-navigation/native";
 
 
 const UserProfileScreen = () => {
 
+    const { getUserDetails } = useOkto() as OktoContextType;
+    const [userDetails, setUserDetails] = useState<User | null>(null);
 
+    const { getWallets } = useOkto() as OktoContextType;
+    const [userWallets, setUserWallets] = useState<Wallet[]>([]);
+
+    const { showWidgetSheet, closeBottomSheet } = useOkto() as OktoContextType;
+
+    useEffect(() => {
+        getUserDetails()
+              .then((result) => {
+                setUserDetails(result);
+              })
+              .catch((error) => {
+                console.error(`error:`, error);
+              });
+    }, []);
+
+    useEffect(() => {
+        getWallets()
+              .then((result) => {
+                setUserWallets(result);
+              })
+              .catch((error) => {
+                console.error(`error:`, error);
+              });
+    }, []);
    
    
 
     return (
-        <View style={styles.container}>
-            
-            <View style={styles.walletsContainer}>
-                <Text style={styles.header}>My Wallets</Text>
-                <Pressable onPress={() => openOktoBottomsheet()}>
-                    <Text style={{ color: "#468cd1", fontSize: 14 }}>Open Okto Widget</Text>
-                </Pressable>
-            </View>
-            
-            <View style={{ flex: 1, paddingHorizontal: 25 }}>
-                
-                <View style={{ gap: 10, flexDirection: 'row', justifyContent: "space-between" }}>
-                   
-                </View>
-                
+        <View style={{ flex: 1, backgroundColor: '#fff' }}>
+            <Text>User Details</Text>
+            {userDetails && <View>
+                <Text>{userDetails.user_id}</Text>
+                <Text>{userDetails.email}</Text>
+            </View>}
+
+            <Button
+                title="Open Okto Profile"
+                onPress={() => {
+                showWidgetSheet();
+                }}
+            />
+
+            <Text>User Wallets</Text>
+            <View>
+                {userWallets.map((wallet, index) => (
+                    <View key={index} style={{ marginVertical: 10 }}>
+                        <Text>{wallet.network_name}</Text>
+                        <Text>{wallet.address}</Text>
+                    </View>
+                ))}
             </View>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
