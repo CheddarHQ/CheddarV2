b20f70e10dbf1c189842b81d26c80d2b3e37505f
Shauray Dhingra <shauraydhingra03@gmail.com>
2024-10-06 02:47:42 +0530

    jeet gaye

--- dumpling/client.ts
+++ dumpling/client.ts
@@ -1,11 +1,11 @@
-import { createClient } from "@dynamic-labs/client";
-import { ReactNativeExtension } from "@dynamic-labs/react-native-extension";
-import { SolanaExtension } from "@dynamic-labs/solana-extension";
+import { createClient } from '@dynamic-labs/client';
+import { ReactNativeExtension } from '@dynamic-labs/react-native-extension';
+import { SolanaExtension } from '@dynamic-labs/solana-extension';
 
 export const dynamicClient = createClient({
-  environmentId: "329c33c2-f35f-428c-89b8-32c4c36c2cd6",
+  environmentId: '0673ae5a-5cbb-4d60-8b13-5edb62181b0a',
   // Optional:
-  appLogoUrl: "https://demo.dynamic.xyz/favicon-32x32.png",
-  appName: "GoPilot APP",
+  appLogoUrl: 'https://demo.dynamic.xyz/favicon-32x32.png',
+  appName: 'GoPilot APP',
   // }).extend(SolanaExtension());
 }).extend(ReactNativeExtension());
